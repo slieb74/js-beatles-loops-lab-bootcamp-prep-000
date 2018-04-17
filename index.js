5121de319970cb3bb2f15f32e2d7521001263bf5
Sam <sammyliebman@gmail.com>
2018-04-17 02:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   let i = 0;
-  while(facts[i] < fact.length - i) {
+  while(facts[i] < facts.length - i) {
     facts[i] += '!!!'
   }
   return facts
